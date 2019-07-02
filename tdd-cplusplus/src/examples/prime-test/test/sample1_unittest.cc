e29a87f828c82432d5ac8d0d04bdda87514aefc8
suraj Nayak <suraj@equalexperts.com>
2019-07-02 12:48:24 +0530

    removed the some code for session

--- tdd-cplusplus/src/examples/prime-test/test/sample1_unittest.cc
+++ tdd-cplusplus/src/examples/prime-test/test/sample1_unittest.cc
@@ -9,36 +9,23 @@ namespace {
 TEST(FactorialTest, Negative) {
   // This test is named "Negative", and belongs to the "FactorialTest"
   // test case.
-  EXPECT_EQ(1, Factorial(-5));
-  EXPECT_EQ(1, Factorial(-1));
-  EXPECT_GT(Factorial(-10), 0);
+
 
 
 }
 
 // Tests factorial of 0.
 TEST(FactorialTest, Zero) {
-  EXPECT_EQ(1, Factorial(0));
+
 }
 
 // Tests factorial of positive numbers.
 TEST(FactorialTest, Positive) {
 
-  SCOPED_TRACE("this is debug");
-  EXPECT_EQ(1, Factorial(1)) << "Actual value for FactorialTest can not be negative" ;
-  EXPECT_EQ(2, Factorial(2));
-  EXPECT_EQ(6, Factorial(3));
-  EXPECT_EQ(40320, Factorial(8));
+
 
 }
 
-
-TEST(SomeTest, Positive) {
-	EXPECT_EQ(1,2);
-	EXPECT_EQ(2,2);
-
-	EXPECT_EQ(3,3);
-}
 
 // Tests IsPrime()
 
@@ -46,26 +33,17 @@ TEST(SomeTest, Positive) {
 TEST(IsPrimeTest, Negative) {
   // This test belongs to the IsPrimeTest test case.
 
-  EXPECT_FALSE(IsPrime(-1));
-  EXPECT_FALSE(IsPrime(-2));
-  EXPECT_FALSE(IsPrime(INT_MIN));
 }
 
 // Tests some trivial cases.
 TEST(IsPrimeTest, Trivial) {
 
 
-  EXPECT_FALSE(IsPrime(0));
-  EXPECT_FALSE(IsPrime(1));
-  EXPECT_TRUE(IsPrime(2));
-  EXPECT_TRUE(IsPrime(3));
+
 }
 
 // Tests positive input.
 TEST(IsPrimeTest, Positive) {
-  EXPECT_FALSE(IsPrime(4));
-  EXPECT_TRUE(IsPrime(5));
-  EXPECT_FALSE(IsPrime(6));
-  EXPECT_TRUE(IsPrime(23));
+
 }
 }
