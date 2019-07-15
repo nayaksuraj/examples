2ddb1f1347d765d09475afadac74a0bc26856439
suraj Nayak <suraj@equalexperts.com>
2019-07-16 01:02:30 +0530

    added spring boot app for elk demo

---
+++ elk-playground/springbootelkdemo/src/main/java/ELKDemoController.java
@@ -0,0 +1,54 @@
+package com.springbootelkdemo;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+class ELKDemoController {
+    private static final Logger LOG = Logger.getLogger(ELKController.class.getName());
+
+    @Autowired
+    RestTemplate restTemplete;
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping(value = "/elk-demo")
+    public String helloWorld() {
+        String response = "Welcome to Spring boot ELK demo : " + new Date();
+        LOG.log(Level.INFO, response);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/exception")
+    public String exception() {
+        String response = "";
+        try {
+            throw new Exception("Exception has occured....");
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOG.error(e);
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            LOG.error("Exception - " + stackTrace);
+            response = stackTrace;
+        }
+
+        return response;
+    }
+}
